d78af2bf6d52c1133235352d9b6715ac4a6d9e26
Aravindchandran98 <aravindkumar04016@gmail.com>
2021-05-28 11:23:57 +0530

    remover server

--- server/routes/api/v1/index.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-const userRouter = require('./users');
-const router = express.Router();
-
-router.use('/', userRouter);
-
-module.exports = router;
--- server/routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const apiRouter = require('./api/v1');
-const router = express.Router();
-
-router.use('/api/v1', apiRouter);
-
-module.exports = router;
